5479e46b9ee5f01306eb66984fd3a527b89a8756
Top-Slayer <dragontops55@gmail.com>
2023-07-16 14:42:47 +0700

    Project just started

---
+++ Introduction Portfolio/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+</script>
+
+<template>
+    
+    <div class="alert alert-primary rounded float-start" role="alert">
+    A simple primary alert—check it out!
+    </div>
+    <div class="alert alert-secondary" role="alert">
+    A simple secondary alert—check it out!
+    </div>
+    <div class="alert alert-success" role="alert">
+    A simple success alert—check it out!
+    </div>
+    <div class="alert alert-danger" role="alert">
+    A simple danger alert—check it out!
+    </div>
+    <div class="alert alert-warning" role="alert">
+    A simple warning alert—check it out!
+    </div>
+    <div class="alert alert-info" role="alert">
+    A simple info alert—check it out!
+    </div>
+    <div class="alert alert-light" role="alert">
+    A simple light alert—check it out!
+    </div>
+    <div class="alert alert-dark" role="alert">
+    A simple dark alert—check it out!
+    </div>
+
+    <h1>sdadasd</h1>
+    <h1 lang="la">ແຊບຫຼາຍ</h1>
+</template>
+
+<style scoped>
+  
+</style>
