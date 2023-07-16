01391372f990725a00ce200b33c84dde715121e2
Top-Slayer <dragontops55@gmail.com>
2023-07-16 16:53:23 +0700

    Introduction

--- Introduction Portfolio/src/App.vue
+++ Introduction Portfolio/src/App.vue
@@ -1,42 +1,21 @@
 <script setup>
-
+    import PortfolioContainer from './components/PortfolioContainer.vue';
 </script>
 
 <template>
-    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
-
-    <div class="alert alert-primary rounded float-start" role="alert">
-    A simple primary alert—check it out!
-    </div>
-    <div class="alert alert-secondary" role="alert">
-    A simple secondary alert—check it out!
-    </div>
-    <div class="alert alert-success" role="alert">
-    A simple success alert—check it out!
-    </div>
-    <div class="alert alert-danger" role="alert">
-    A simple danger alert—check it out!
-    </div>
-    <div class="alert alert-warning" role="alert">
-    A simple warning alert—check it out!
-    </div>
-    <div class="alert alert-info" role="alert">
-    A simple info alert—check it out!
-    </div>
-    <div class="alert alert-light" role="alert">
-    A simple light alert—check it out!
-    </div>
-    <div class="alert alert-dark" role="alert">
-    A simple dark alert—check it out!
-    </div>
+    
+    <PortfolioContainer />
 
     <h1>sdadasd</h1>
     <h1 lang="la">ແຊບຫຼາຍ</h1>
     
-    <span class="material-symbols-outlined">
-        menu
-    </span>
-    <span class="material-symbols-outlined" style="color: hsl(176, 100%, 50%)">
+    <button>
+        <span class="material-symbols-outlined">
+            menu
+        </span>
+    </button>
+
+    <span class="material-symbols-outlined" style="color: hsl(208, 94%, 52%);">
         search
     </span>
 </template>
---
+++ Introduction Portfolio/src/components/PortfolioContainer.vue
@@ -0,0 +1,9 @@
+<script setup>
+    import Introduction from './sections/Introduction.vue';
+</script>
+
+<template>
+    <div class="container fluid py-4">
+        <Introduction />
+    </div>
+</template>
