8feeef28ef803860e1b705e985e20bea1b7c249f
Top-Slayer <dragontops55@gmail.com>
2023-07-16 16:53:23 +0700

    Introduction

---
+++ Introduction Portfolio/src/components/sections/Introduction.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="row align-items-center">
+        <div class="col-6">
+            <img src="/images/Prop image.svg" width="200">
+        </div>
+        <div class="col-6">
+            <div>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores ducimus accusantium iure laudantium expedita maxime reiciendis, provident commodi quo iusto, consectetur veritatis mollitia aspernatur. Unde, dolore! Autem illo unde consectetur!
+            </div>
+            <div class="text-end">
+                <h1>asdadasdsdsdsasd</h1>
+            </div>
+        </div>
+    </div>
+</template>
