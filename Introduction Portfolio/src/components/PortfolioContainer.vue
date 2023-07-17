64ad5bad9acd1ba5d114ceeb9f55baa0afc27ed3
Top-Slayer <dragontops55@gmail.com>
2023-07-17 15:11:00 +0700

    Error

--- Introduction Portfolio/src/components/PortfolioContainer.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-    import Introduction from './sections/Introduction.vue';
-</script>
-
-<template>
-    <div class="container fluid py-4">
-        <Introduction />
-    </div>
-</template>
