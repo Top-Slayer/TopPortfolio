06a0b88cd340f4467bca75369d16dc86ed851aae
Top-Slayer <dragontops55@gmail.com>
2023-07-23 14:39:49 +0700

    a little Modified

--- Introduction Portfolio/src/components/sections/Menubar.vue
+++ Introduction Portfolio/src/components/sections/Menubar.vue
@@ -1,55 +1,59 @@
+<script setup>
+  import PortfolioContainerTutorial from '../PortfolioContainerTutorial.vue';
+</script>
+
 <template>
   <div
-    class="text-center position-fixed"
-    style="
+  class="text-center position-fixed"
+  style="
       padding-top: 110px;
       width: 60px;
       height: 1000%;
-      background: #24292c;
+      background: linear-gradient(to right, #24292c 90%,#34474D 10%);
       box-shadow: 10px 0px 50px 10px rgba(0, 0, 0, 0.5);
     "
   >
-    <div>
+  <div>
       <!-- home button icon -->
-      <div class="py-4">
-        <span class="material-symbols-outlined"
-          ><a
-            id="transition"
-            href="#"
-            class="link-offset-2 link-underline link-underline-opacity-0"
+        <div class="py-4">
+          <span class="material-symbols-outlined"
+            ><a
+              id="transition"
+              href="#"
+              class="link-offset-2 link-underline link-underline-opacity-0"
+              >
+              home
+            </a></span
+            >
+          </div>
+  
+        <!-- widgets button icon -->
+        <div class="py-4">
+          <span class="material-symbols-outlined"
+            ><a
+              id="transition"
+              href="#"
+              class="link-offset-2 link-underline link-underline-opacity-0"
+            >
+              widgets
+            </a></span
           >
-            home
-          </a></span
-        >
-      </div>
-
-      <!-- widgets button icon -->
-      <div class="py-4">
-        <span class="material-symbols-outlined"
-          ><a
-            id="transition"
-            href="#"
-            class="link-offset-2 link-underline link-underline-opacity-0"
+        </div>
+  
+        <!-- game button icon -->
+        <div class="py-4">
+          <span class="material-symbols-outlined"
+            ><a
+              id="transition"
+              href="#"
+              class="link-offset-2 link-underline link-underline-opacity-0"
+            >
+              videogame_asset
+            </a></span
           >
-            widgets
-          </a></span
-        >
-      </div>
-
-      <!-- game button icon -->
-      <div class="py-4">
-        <span class="material-symbols-outlined"
-          ><a
-            id="transition"
-            href="#"
-            class="link-offset-2 link-underline link-underline-opacity-0"
-          >
-            videogame_asset
-          </a></span
-        >
+        </div>
       </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
